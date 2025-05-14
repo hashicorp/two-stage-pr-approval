d27e65200d663c4dbb9b87e18716e4ebbc70d661
ritikrajdev <56875929+ritikrajdev@users.noreply.github.com>
2025-05-14 10:28:37 +0530

    chore: add debug flag in script and action

--- scripts/pr-details.sh
+++ scripts/pr-details.sh
@@ -4,19 +4,35 @@
 # Usage: ./pr-details.sh <PR_NUMBER> <TEAM_MEMBERS>
 
 # Check if the required arguments are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <PR_NUMBER> <TEAM_MEMBERS>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <PR_NUMBER> <TEAM_MEMBERS> <DEBUG>"
     exit 1
 fi
 
 PR_NUMBER=$1
 TEAM_MEMBERS=$2
+DEBUG=false
+if [ "$3" == "true" ]; then
+    DEBUG=true
+fi
 
-pwd
+echoerr() {
+    # Only used for debugging
+    if $DEBUG; then
+        echo "$@" 1>&2
+    fi
+}
+
+if $DEBUG; then
+    echoerr "Debugging enabled"
+    pwd 1>&2
+fi
+    
 
 # Get PR creator
 PR_CREATOR=$(gh pr view $PR_NUMBER --json author --jq .author.login)
 echo "PR_CREATOR=$PR_CREATOR"
+echoerr "PR_CREATOR=$PR_CREATOR"
 
 
 # Check Team Membership
@@ -25,6 +41,7 @@ if [[ $TEAM_MEMBERS == *"$PR_CREATOR"* ]]; then
 else
     echo "IS_TEAM_MEMBER=false"
 fi
+echoerr "IS_TEAM_MEMBER=$IS_TEAM_MEMBER"
 
 
 # Check if PR has internal-approval label
@@ -34,11 +51,13 @@ if [[ $LABELS == *"internal-approval"* ]]; then
 else
     echo "HAS_APPROVAL_LABEL=false"
 fi
+echoerr "HAS_APPROVAL_LABEL=$HAS_APPROVAL_LABEL"
 
 
 # Check if PR is in draft state
 IS_DRAFT=$(gh pr view $PR_NUMBER --json isDraft --jq .isDraft)
 echo "IS_DRAFT=$IS_DRAFT"
+echoerr "IS_DRAFT=$IS_DRAFT"
 
 
 # Check for team member approvals (excluding self-approval)
@@ -51,3 +70,4 @@ if [[ $TEAM_MEMBERS == *"$REVIEWER"* && "$REVIEWER" != "$PR_CREATOR" ]]; then
 fi
 done
 echo "HAS_TEAM_APPROVAL=$HAS_TEAM_APPROVAL"
+echoerr "HAS_TEAM_APPROVAL=$HAS_TEAM_APPROVAL"
